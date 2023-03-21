410665bc062893172025c06f83d250c4d8416425
loicbefve <befve.loic@gmail.com>
2023-03-21 22:45:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Description :
+
+API du site de la bibliothèque.
+
+## Technologies :
+- Express.js (Serveur http)
+- Fuse.js (Moteur de recherche léger)
---
+++ db/index.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+const config = require('../config/dbConfig');
+
+const pool = mysql.createPool({
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  database: config.database,
+});
+
+// Export the pool object to use in other parts of your application
+module.exports = pool.promise();
