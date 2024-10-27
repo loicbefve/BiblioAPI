ad1a684cdb3d90890b5cc42de247808bc176b406
lbefve <loic.befve@gensdeconfiance.com>
2024-10-27 14:19:24 +0100

    feat: add the possibility to specify the wanted port to connect

--- app.ts
+++ app.ts
@@ -50,5 +50,5 @@ const errorHandler: ErrorRequestHandler = (err: Error, req: Request, res: Respon
 };
 app.use(errorHandler);
 
-app.listen(3000);
+app.listen(config.apiPort || 3000);
 console.log('Server started on port 3000');
