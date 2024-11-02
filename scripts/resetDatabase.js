8a22165fbc308ac1cd74e285ed53dfad3a56c78d
lbefve <loic.befve@gensdeconfiance.com>
2024-11-02 22:41:15 +0100

    feat: use DBNAME env var

--- scripts/resetDatabase.js
+++ scripts/resetDatabase.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const pool = new Pool({
   user: process.env.POSTGRES_USER || 'root',
   host: process.env.DB_HOST,
-  database: 'postgres',
+  database: process.env.DB_NAME,
   password: process.env.POSTGRES_PASSWORD || 'root',
 });
 
